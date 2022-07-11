6bb0965baf0d2b8b9d998dfe54c0e6550e9cfd88
iandrew8 <ibandaandrew8@gmail.com>
2022-07-11 23:34:26 +0300

    added keys

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const ALCHEMY_API_KEY = "J8r4GTuoQSMKX7H8Kj3IPL4Cl4An1gUY";
+
+const GOERLI_PRIVATE_KEY = "0x40eb55eb96a7c5012bdd88b2b65b448dda79f536f94e9432d8e0b13585c5ca4f";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
